9e13dc850cc7438b9fd4e274f6d4ce0a18734caf
StephenFitz1 <93268638+StephenFitz1@users.noreply.github.com>
2022-11-05 17:18:35 +0000

    Add files via upload

---
+++ CS4004Project/src/Meetings.java
@@ -0,0 +1,51 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class Meetings {
+    //Plenty more to add but just to get a start
+    LocalDateTime timeOfMeeting;
+    boolean prof, priv, pub;
+
+    public Meetings(LocalDateTime meetingStart, ArrayList<Person> personList, boolean prof, boolean priv){
+    this.prof = prof;
+
+    this.priv = priv;
+
+    }
+
+    public void createMeetings(Person person, LocalDateTime dateStartOfRange, LocalDateTime dateEndOfRange){
+        //Include type of meeting, Professional, private, public meeting etc
+        boolean pro, priv, pub;
+    }
+
+    public void inviteToMeeting(ArrayList<Person> person){
+
+    }
+
+    public void returnRequirements(){ //What devices are needed etc
+    }
+
+    /*
+      For this we need to make it so there can be constraints put on the meeting
+      i,e time, but also allow flexibility.
+      Also needs to be able to use both formats for dates
+      DD-MM-YYYY(standard) and MM-DD-YYYY(american).
+      Non-privileged participants shouldn't be aware of constraints, only higher ups
+       */
+    public void meetingTimePreferences(Person person){
+    }
+
+
+    public void getPreferences(Person person, boolean inOffice){
+        //Important people may be asked to state preference of location
+        //For ease we'll just do two locations, office 'A' or something like that
+        //and online and use 'Zoom' or whatever
+    }
+
+
+
+
+
+
+
+}
---
+++ CS4004Project/src/Person.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+
+public class Person {
+
+    String name;
+    String title;
+    boolean meetingInOffice;
+    private ArrayList<Person> persons = new ArrayList<Person>();
+
+    Person(){
+
+    }
+
+    Person(String name, String title, boolean meetingInOffice){
+        this.name = name;
+        this.title = title;
+        this.meetingInOffice = meetingInOffice;
+    }
+
+    public boolean meetingPlacePreference(boolean inOffice){
+        if(inOffice){
+            System.out.println("Meeting shall be in the office");
+            return true;
+        } else {
+            System.out.println("Lazy cunt, home it is");
+        }
+        return false;
+    }
+    public void addToPersons(Person person){
+        persons.add(person);
+    }
+
+    public ArrayList<Person> getPersons() {
+        return persons;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setMeetingInOffice(boolean meetingInOffice) {
+        this.meetingInOffice = meetingInOffice;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPersons(ArrayList<Person> persons) {
+        this.persons = persons;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+//    public static void main(String[] args) {
+//        Person p1 = new Person("Junky Joe Joyce", "Top shagger", true);
+//        System.out.println(p1.getName());
+//        System.out.print(p1.getTitle());
+//    }
+}
